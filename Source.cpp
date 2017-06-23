5e863d50a597ad79f592115f24c22f9b7bd7edf7
Jowan9 <i.vodyaniy@student.csn.khai.edu>
2017-06-23 23:35:40 +0300

    add func that inverse string

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include<string.h>
+#define MAX_LENGTH 500
+
+void should_return_inverse_string ();
 char str[255];
 int scopes(char str1[255]){
 	bool any = false;
@@ -41,9 +45,30 @@ int scopes(char str1[255]){
 		return -1;
 	}
 }
+
 int main(void){
+	
 	gets(str);
 	int res = scopes(str);
 	printf("res: %d\n%s\n", res, str);
+	printf("Now the inverce_string function will execute...\n");
+	should_return_inverse_string();
 	return 0;
 }
+
+void should_return_inverse_string ()
+
+{
+	char initial_string[MAX_LENGTH]={'\0'};
+	char inverse_string[MAX_LENGTH]={'\0'};
+	
+	printf("Input initial string:");
+	fgets(initial_string, MAX_LENGTH, stdin);
+	
+	for(int i=strlen(initial_string)-1;  i!=-1; i--)
+	{
+		strncat(inverse_string,initial_string+i,1);
+	}
+	puts(inverse_string);
+}
+
